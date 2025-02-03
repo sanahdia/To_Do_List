de4429d4b18a4f84293d3cc25703c69b9679761e
Sana <158482751+sanahdia@users.noreply.github.com>
2025-02-03 12:14:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// JavaScript
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+  
+    if (taskInput.value.trim() === '') {
+      alert('Please enter a task!');
+      return;
+    }
+  
+    const li = document.createElement('li');
+    li.textContent = taskInput.value;
+  
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.onclick = function () {
+      taskList.removeChild(li);
+    };
+  
+    li.appendChild(deleteButton);
+    taskList.appendChild(li);
+  
+    taskInput.value = '';
+  }
